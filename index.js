8e2e398fcce6682a797b55144b72eda89fd7ac2c
Wafaa-AbdElHafez-Ahmed <wafaa.abdelhafez-ahmed@vodafone.com>
2019-11-08 20:20:08 +0200

    Added second api 'mine' a block to be added to the blockchain

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
+const bodyParser = require('body-parser');
 const express = require('express');
 const BlockChain = require('./blockchain');
 
 const app = express();
 const blockChain = new BlockChain();
 
+app.use(bodyParser.urlencoded({
+    extended: true
+  }));
+app.use(bodyParser.json());
+
 app.get('/api/blocks', (req, res) => {
     res.json(blockChain.chain);
+});
+
+app.post('/api/mine', (req, res) => {
+    const { data } = req.body;
+    blockChain.addBlock({ data });
+    res.redirect('/api/blocks');
 });
 
 const PORT = 3000;
