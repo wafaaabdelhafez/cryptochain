d6037b73cd36f50d919a46ec478933d688026eeb
Wafaa-AbdElHafez-Ahmed <wafaa.abdelhafez-ahmed@vodafone.com>
2019-11-10 16:17:04 +0200

    Added cross-env library to handle multible instances of the app

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const BlockChain = require('./blockchain');
+const PubSub = require('./pubsub');
 
 const app = express();
-const blockChain = new BlockChain();
+const blockchain = new BlockChain();
+const pubsub = new PubSub({ blockchain });
+
+setTimeout(() => pubsub.broadcastChain(), 2000);
 
 app.use(bodyParser.urlencoded({
     extended: true
@@ -11,14 +15,21 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json());
 
 app.get('/api/blocks', (req, res) => {
-    res.json(blockChain.chain);
+    res.json(blockchain.chain);
 });
 
 app.post('/api/mine', (req, res) => {
     const { data } = req.body;
-    blockChain.addBlock({ data });
+    blockchain.addBlock({ data });
     res.redirect('/api/blocks');
 });
 
-const PORT = 3000;
+const DEFAULT_PORT = 3000;
+let PERR_PORT;
+
+if(process.env.GENERATE_PEER_PORT === 'true'){
+    PERR_PORT = DEFAULT_PORT + Math.ceil(Math.random() * 1000);
+}
+
+const PORT = PERR_PORT || DEFAULT_PORT;
 app.listen(PORT, () => { console.log(`listining at localhost: ${PORT}`) });
